aba2070a8c3532ac72177e34f22ee871c4fccae8
Yash <yashsingh3531@gmail.com>
2025-09-02 16:36:52 +0530

    Delete server directory

--- server/middleware/multer.js
+++
@@ -1,9 +0,0 @@
-import multer from "multer";
-
-const storage =multer.diskStorage({
-    filename:function(re,file,callback){
-        callback(null,`${Date.now()}_${file.originalname}`)
-    }
-})
-
-export const upload=multer({storage})
